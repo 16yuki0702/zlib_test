79794ad91850488e25a32c5f6b852237b45db99b
16yuki0702 <hs19870702@gmail.com>
2017-07-07 01:56:23 +0000

    [add]settings, headers, and test code.

--- src/main.c
+++ src/main.c
@@ -6,5 +6,6 @@ main(int argc, char *argv[])
 	z_stream strm = { .zalloc = Z_NULL, .zfree = Z_NULL, .opaque = Z_NULL };
 	deflateInit(&strm, Z_DEFAULT_COMPRESSION);
 	deflateEnd(&strm);
+	printf("test\n");
 	return 0;
 }
